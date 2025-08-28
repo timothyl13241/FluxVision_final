d83d0841abbe89f34f04f2f448ea6e4649fb119f
Timothy Le <14115751+timothyl13241@users.noreply.github.com>
2025-08-27 20:49:58 -0700

    Port all scripts to Python 3 syntax and compatibility

--- skip_video.py
+++ skip_video.py
@@ -1,45 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import RPi.GPIO as GPIO
 from evdev import uinput, ecodes as e
 import time
 
-#define skip button GPIO pin
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP) #pin 40
-
-#define LED GPIO pin
-GPIO.setup(13, GPIO.OUT) #pin 33
-GPIO.output(13, True)
-
-prev_inp = 1
-
 def skip_video(PinNr):
-	global prev_inp
+    global prev_inp
 
-	inp = GPIO.input(PinNr)
-	if ((not prev_inp) and inp):
+    inp = GPIO.input(PinNr)
+    if (not prev_inp) and inp:
 
-		#turn off LED
-		GPIO.output(13,False)
+        #turn off LED
+        GPIO.output(13, False)
 
-		#simulate keystroke "o", which tells omxplayer to skip to the next video (easier than using dbus!)
-		with uinput.UInput() as ui:
-			ui.write(e.EV_KEY, e.KEY_O, 1)
-			ui.syn()
+        #simulate keystroke "o", which tells omxplayer to skip to the next video (easier than using dbus!)
+        with uinput.UInput() as ui:
+            ui.write(e.EV_KEY, e.KEY_O, 1)
+            ui.syn()
 
-		#disble button for 1 second
-		time.sleep(1.0)
+        #disable button for 1 second
+        time.sleep(1.0)
 
-		#turn on LED
-		GPIO.output(13,True)
+        #turn on LED
+        GPIO.output(13, True)
 
-	prev_inp = inp
+    prev_inp = inp
 
 try:
-	while True:
-		skip_video(21)
+    while True:
+        skip_video(21)
 except KeyboardInterrupt:
-	GPIO.cleanup()
+    GPIO.cleanup()
